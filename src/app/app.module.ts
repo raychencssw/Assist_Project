763012bd52edc74a4bffd7a4604b639d4c0a783b
Jack Lu <jackluzhouqian@gmail.com>
2023-05-21 11:19:37 -1000

    Add create event, event details, qr scanner and profile to routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { EventsComponent } from './events/events.component';
 import { RankingsComponent } from './rankings/rankings.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { EventdetailsComponent } from './eventdetails/eventdetails.component';
+import { EventcreateComponent } from './eventcreate/eventcreate.component';
+import { QrscannerComponent } from './qrscanner/qrscanner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     EventsComponent,
-    RankingsComponent
+    RankingsComponent,
+    ProfileComponent,
+    EventdetailsComponent,
+    EventcreateComponent,
+    QrscannerComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
