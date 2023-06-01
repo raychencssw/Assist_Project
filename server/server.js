72864240984c35782c6cb3ff1af7d836f87f19aa
Jack Lu <jackluzhouqian@gmail.com>
2023-06-02 00:09:50 +0800

    Add signup page and login page to the app, add signup route to the server

--- server/server.js
+++ server/server.js
@@ -1,41 +1,43 @@
-const express= require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require('path')
-const mongoose = require('mongoose')
-const User = require('./models/user')
+const path = require("path");
+const mongoose = require("mongoose");
+const User = require("./models/user");
+//const passport = require("passport");
+//const initializePassport = require("./passport-config");
+//initializePassport(passport);
 // const ngeohash = require('ngeohash')
 
-const posts = []
+const posts = [];
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({origin: true, credentials: true}))
+app.use(cors({ origin: true, credentials: true }));
 
-let db_url = 'mongodb+srv://jitbaner:4r17oq9ZuznScSih@cluster0.znvt1pl.mongodb.net/AssistProject?retryWrites=true&w=majority'
+let db_url =
+  "mongodb+srv://jitbaner:4r17oq9ZuznScSih@cluster0.znvt1pl.mongodb.net/AssistProject?retryWrites=true&w=majority";
 
+mongoose
+  .connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("CONNECTION OPEN");
+  })
+  .catch((e) => {
+    console.log("Error!");
+    console.log(e);
+  });
 
-mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() =>{
-        console.log("CONNECTION OPEN")
-    })
-    .catch((e) =>{
-        console.log("Error!");
-        console.log(e);
-    })
+app.post("/posts/submit", (req, res) => {
+  const data = req.body.post;
+  posts.push(data);
+  console.log(posts);
 
-app.post('/posts/submit', (req, res)=>{
-    const data = req.body.post
-    posts.push(data)
-    console.log(posts)
-
-    res.json({success: true, message: "Data received"})
-
-
-})
+  res.json({ success: true, message: "Data received" });
+});
 
 // app.get('/signup', async (req, res)=>{
 //     const user = new User({id: 1, username: 'test', email: 'test@test.com', firstname: 'testFirst', lastname: 'testLast', role: 1, points: 14})
@@ -43,45 +45,73 @@ app.post('/posts/submit', (req, res)=>{
 //     res.send(user)
 // })
 
-app.post('/signup', (req, res)=>{
-    // Jack should work here. Receive the userdata and store it in the "User" collection.
-})
+app.post("/signup", async (req, res) => {
+  // Jack should work here. Receive the userdata and store it in the "User" collection.
 
-app.post('/login', (req, res)=>{
-    // receive the data here and check if the user exists if it does return a random response. As of now nothing more than this.
-})
+  try {
+    const {
+      username,
+      email,
+      firstname,
+      lastname,
+      profilepicture,
+      role,
+      school,
+    } = req.body;
 
+    //Check if username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ message: "Username already exists" });
+    }
 
+    const newUser = new User({
+      username,
+      email,
+      firstname,
+      lastname,
+      profilepicture,
+      role,
+    });
+    await newUser.save();
+    console.log("user saved");
+    res.status(201).json({ message: "User created" });
+  } catch (error) {
+    console.error("Error signing up: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
-app.get('/home', (req, res)=>{
-    res.send({posts: posts})
-})
+app.post("/login", async (req, res) => {
+  res.send("Login successful");
+});
 
-app.get('/qrscan', (req, res)=>{
-    res.send("HERE WE WILL HAVE QR SCANNER")
-})
+app.get("/home", (req, res) => {
+  res.send({ posts: posts });
+});
 
+app.get("/qrscan", (req, res) => {
+  res.send("HERE WE WILL HAVE QR SCANNER");
+});
 
-app.get('/eventdetails/:eventid', (req, res)=>{
-    res.send("HERE WE HAVE EVENT DETAILS")
-})
+app.get("/eventdetails/:eventid", (req, res) => {
+  res.send("HERE WE HAVE EVENT DETAILS");
+});
 
+app.post("/createevent", (req, res) => {
+  res.send("THIS IS NEW EVENT POST");
+});
 
-app.post('/createevent', (req, res)=>{
-    res.send("THIS IS NEW EVENT POST")
-})
+app.get("/profle/:userid", (req, res) => {
+  res.send("HERE IS MY PROFILE");
+});
 
-app.get('/profle/:userid', (req, res)=>{
-    res.send("HERE IS MY PROFILE")
-})
+app.get("/rankings", (req, res) => {
+  res.send("THIS IS RANKINGS");
+});
 
-app.get('/rankings', (req, res)=>{
-    res.send("THIS IS RANKINGS")
-})
+const port = process.env.PORT || 3080;
 
-
-const port= process.env.PORT || 3080;
-
-app.listen(port, () =>{
-    console.log(`Server started at ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server started at ${port}`);
+});
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Define the request body
+  user = {
+    username: '',
+    password: ''
+  };
 
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    // Define the endpoint URL
+    const url = 'http://localhost:3080/login';
+
+    // Send the POST request
+    this.http.post(url, this.user).subscribe(
+      (response) => {
+        // Handle the successful response
+        console.log(response);
+      },
+      (error) => {
+        // Handle any errors
+        console.error(error);
+      }
+    );
+  }
 }
