513d0f2895ed7d36730d2f7825934081cc21d729
jitbanerjee4 <jitbanerjee4@gmail.com>
2023-06-13 02:27:14 -0700

    merged with jack

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 // import { RouterModule, Routes } from '@angular/router';
@@ -15,24 +14,6 @@
 //   { path: 'profile/:id', component: ProfileComponent },
 //   { path: 'events', component: EventsComponent }
 // ]
-=======
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './profile/profile.component';
-import { EventcreateComponent } from './eventcreate/eventcreate.component';
-import { EventdetailsComponent } from './eventdetails/eventdetails.component';
-import { QrscannerComponent } from './qrscanner/qrscanner.component';
-import { EventsComponent } from './events/events.component';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-const routes: Routes = [
-  { path: 'qrscanner', component: QrscannerComponent },
-  { path: 'eventdetails', component: EventdetailsComponent },
-  { path: 'eventcreate', component: EventcreateComponent },
-  { path: 'profile/:id', component: ProfileComponent },
-  { path: 'events', component: EventsComponent }
-]
->>>>>>> origin/ChengShi
 
 
 // @NgModule({
