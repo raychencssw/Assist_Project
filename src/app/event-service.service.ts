ffc0ce5254b343663c8440d5258c23ef3c99a76a
raychencssw <122491800+raychencssw@users.noreply.github.com>
2023-06-17 16:12:41 -0700

    fix bug: events list will be updated after creating a new event

--- src/app/event-service.service.ts
+++ src/app/event-service.service.ts
@@ -8,11 +8,11 @@ import { Observable } from 'rxjs';
 })
 
 export class EventServiceService {
-    events: any = []
+    events: any = [];
   
     constructor(private http: HttpClient, private router: Router) { }
   
-    addtoEvents(event: any){
+    addtoEvents(event: any):Observable<any>{
       console.log("event: " + JSON.stringify(event));
       //event: {"name":"Great event","date":"Great Day","time":"Great Time","location":"Great Locale","description":"Have fun"}
       
@@ -21,14 +21,14 @@ export class EventServiceService {
       //http.post makes a POST request to the backend and returns an Obervable that represents the ongoing HTTP request and allows you to handle the response asynchronously
       //.subscribe observes any changes it subscribe to and specify what should happen when the response is received
       //http.get returns Observable, yet it returns subscription after .subscribe
-      return this.http.post(`http://localhost:3080/createevent`, event).subscribe(()=>{
-        this.router.navigate(['/events']);  //redirect the user back to their events page
-      })
+      console.log("addEvents!")
+      return this.http.post(`http://localhost:3080/createevent`, event);
     }
 
 
     loadEvent(){        
-        return this.http.get(`http://localhost:3080/events`)
+      console.log("loadEvent!")
+      return this.http.get(`http://localhost:3080/events`);
     }
 
     getEventById(eventId: string): Observable<any> {
--- src/app/eventcreate/eventcreate.component.ts
+++ src/app/eventcreate/eventcreate.component.ts
@@ -12,12 +12,18 @@ import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 })
 export class EventcreateComponent implements OnInit{
 
-  // @ViewChild('picker') picker!: MatDatepicker<any>;
   datePickerStartAt:any;
 
-  //2 ways: transform string date into month and date respectively 
-  //1. before store them to the database(defined at eventcreate.component.ts & need to modify event.js schema)
-  //2. after access them from the database(defined at event.component.ts)
+  /**
+   *   Date handling: The user's input was string originally and was assigned to formDate. Then it is passed
+   *                  to Date object to allow further operation. A method toLocaleDateString() is applied to
+   *                  the Date object to transform it to local string format. In the end, the whole form along
+   *                  with the transformed date is sent to backend through eventService.addtoEvents()
+   **/
+
+
+  //used to receive from the user input, then it'll be passed into Date object and be 
+  //transformed to local string format before sending to backend.
   public formDate = "";
 
   public eventForm = {
@@ -45,7 +51,7 @@ export class EventcreateComponent implements OnInit{
   submit(){
 
     console.log("submit button clicked!");
-    
+
     //construct Date object by injecting input of user
     let tempDate = new Date(this.formDate);
     console.log("tempDate: " + tempDate);  //Fri Apr 07 2023 00:00:00 GMT-0700 (Pacific Daylight Time)
@@ -56,10 +62,11 @@ export class EventcreateComponent implements OnInit{
     console.log("formattedDate: " + formattedDate);    //formattedDate: 4/7/2023
 
     //call eventService and then store the event info to the MongoDB
-    this.eventService.addtoEvents(this.eventForm)
+    this.eventService.addtoEvents(this.eventForm).subscribe(() => {
+      // Close the modal
+      this.activeModal.close();
+    })
 
-    // Close the modal
-    this.activeModal.close();
   }
 
 }
