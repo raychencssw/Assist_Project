decb0e8a19be8069f84c901e774e11351b43c0a2
raychencssw <122491800+raychencssw@users.noreply.github.com>
2024-11-05 00:47:51 -0800

    remove duplicates

---
+++ src/app/landing/landing.component 2.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent implements OnInit {
+  @ViewChild('video') video: any;
+  videoUrl?: SafeResourceUrl;
+  constructor(private router: Router, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/production_id_4508061 (1080p).mp4');
+  }
+
+  redirectSignup() {
+    this.router.navigate(['signup'])
+  }
+  redirectLogin() {
+    this.router.navigate(['login'])
+  }
+  redirectsupervisorLogin() {
+    this.router.navigate(['supervisorlogin'])
+  }
+}
